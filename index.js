944612caea22d90f0bb99a043b711e959c08111e
Mahmud2123 <92996758+Mahmud2123@users.noreply.github.com>
2024-08-14 01:16:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,16 +52,33 @@ function getWeather(city, unit) {
 
 function getUserLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            const { latitude, longitude } = position.coords;
-            getWeatherByCoordinates(latitude, longitude);
-        }, () => {
-            alert('Unable to retrieve your location.');
-        });
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                getWeatherByCoordinates(latitude, longitude);
+            },
+            (error) => {
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        alert('User denied the request for Geolocation.');
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        alert('Location information is unavailable.');
+                        break;
+                    case error.TIMEOUT:
+                        alert('The request to get user location timed out.');
+                        break;
+                    case error.UNKNOWN_ERROR:
+                        alert('An unknown error occurred.');
+                        break;
+                }
+            }
+        );
     } else {
         alert('Geolocation is not supported by this browser.');
     }
 }
+
 
 function getWeatherByCoordinates(lat, lon) {
     const unit = document.getElementById('unitSelect').value;
